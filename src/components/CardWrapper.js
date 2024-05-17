ea8f206f687fcb9f3b52e2732e129d7c94a410f2
samiursakib <rafisamiur@gmail.com>
2024-05-18 02:29:05 +0600

    random rotation fixed

--- src/components/CardWrapper.js
+++ src/components/CardWrapper.js
@@ -28,18 +28,21 @@ const CardWrapper = ({ id, card, player1, setPlayer1 }) => {
 
   // const scale = isThrown ? 'scale(1.1)' : '';
 
-  const rotate = ` rotate(${isThrown ? 45 : findRotationAngle(id)}deg)`;
+  const rotate = ` rotate(${
+    isThrown ? parseInt(Math.random() * 360) : findRotationAngle(id)
+  }deg)`;
 
   const cardThrow = () => {
     setIsThrown((prev) => !prev);
 
-    // setPlayer1((prev) => ({
-    //   ...prev,
-    //   leftCards: prev.leftCards.filter((c) => c !== card),
-    //   usedCards: [...prev.usedCards, card],
-    // }));
+    //   setPlayer1((prev) => ({
+    //     ...prev,
+    //     leftCards: prev.leftCards.filter((c) => c !== card),
+    //     usedCards: [...prev.usedCards, card],
+    //   }));
   };
 
+  console.log(isThrown, rotate);
   return (
     <div
       ref={ref}
